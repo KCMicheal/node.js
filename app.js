4c431c326e83c92bd4e17f0ff1518e8e05d1b7bc
KCMicheal <kcmicheal@users.noreply.github.com>
2021-06-15 15:46:49 +0100

    ejs added

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ app.listen(3000);
 app.get('/', (req, res) => {
     console.log('connected!!!')
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index', { title: 'Home'})
 })
 
 app.get('/about', (req, res) => {
     console.log('connected!!!')
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'About'})
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create New Blog'})
 })
 
 //redirects
@@ -29,5 +33,5 @@ app.get('/about-us', (req, res) => {
 
 //404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', { title: '404'})
 })
