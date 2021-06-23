f43eb47867823e11414f33b95627544a167f3668
KCMicheal <kcmicheal@users.noreply.github.com>
2021-06-23 18:20:23 +0100

    mongodb connect

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //express app
 const app = express();
 
 //connect to mongoDB
 const dbURI = 'mongodb+srv://netninja:adgjmptw@sandbox.yboau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err))
 //register view engine
 app.set('view engine', 'ejs');
 
